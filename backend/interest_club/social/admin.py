252e56b84dbbeba98a06d18a6892c4041c5f35f8
litpa <aslitpa@gmail.com>
2024-05-23 18:39:02 +0700

    Move comments to the record admin page

--- backend/interest_club/social/admin.py
+++ backend/interest_club/social/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import SocialGroup, GroupRecord, Comment
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+class GroupRecordAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["author", "name", "description"]}),
+        ("Creation date", {"fields": ["created_date"], "classes": ["collapse"]}),
+        ("Event date", {"fields": ["event_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [CommentInline]
+
 admin.site.register(SocialGroup)
-admin.site.register(GroupRecord)
-admin.site.register(Comment)
+admin.site.register(GroupRecord, GroupRecordAdmin)
