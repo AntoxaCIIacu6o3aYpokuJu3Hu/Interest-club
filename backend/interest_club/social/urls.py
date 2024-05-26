9b3616186f6025164169c7aa36536e481b7efd73
litpa <aslitpa@gmail.com>
2024-05-26 20:56:49 +0700

    add endpoint for working with group records and comments

--- backend/interest_club/social/urls.py
+++ backend/interest_club/social/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('groups/', views.SocialGroupList.as_view()),
     path('groups/membership/', views.SocialGroupMembership.as_view()),
     path('groups/<str:pk>/', views.SocialGroupDetail.as_view()),
+    path('records/', views.GroupRecordList.as_view()),
+    path('records/<str:pk>/', views.GroupRecordDetail.as_view()),
+    path('comments/', views.CommentList.as_view()),
+    path('comments/<str:pk>/', views.CommentDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/interest_club/social/views.py
+++ backend/interest_club/social/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
@@ -8,7 +10,9 @@ from rest_framework import permissions
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from social.models import SocialGroup, GroupRecord, Comment
-from social.serializers import SocialGroupSerializer, SocialGroupMembershipSerializer
+from social.serializers import SocialGroupSerializer, SocialGroupIdSerializer
+from social.serializers import GroupRecordSerializer, GroupRecordNoGroupSerializer
+from social.serializers import CommentSerializer, CommentNoParentSerializer
 from social.permissions import IsOwnerOrReadOnly
 
 
@@ -52,7 +56,7 @@ class SocialGroupDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class SocialGroupMembership(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = SocialGroupMembershipSerializer
+    serializer_class = SocialGroupIdSerializer
 
     @extend_schema(summary="Join social group by id")
     def post(self, request, format=None):
@@ -76,3 +80,78 @@ class SocialGroupMembership(APIView):
         group.save()
         return Response(request.data, status=status.HTTP_204_NO_CONTENT)
     
+
+@extend_schema_view(
+    get=extend_schema(
+        summary="Get a list of all group records",
+    ),
+    post=extend_schema(
+        summary="Create a new group record",
+    ),
+)
+class GroupRecordList(generics.ListCreateAPIView):
+    queryset = GroupRecord.objects.all()
+    serializer_class = GroupRecordSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(owner=user, created_date=datetime.datetime.now().isoformat())
+    
+
+@extend_schema_view(
+    get=extend_schema(
+        summary="Get group record by id",
+    ),
+    put=extend_schema(
+        summary="Update group record by id",
+    ),
+    patch=extend_schema(
+        summary="Partialy update group record by id",
+    ),
+    delete=extend_schema(
+        summary="Delete group record by id",
+    ),
+)
+class GroupRecordDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GroupRecord.objects.all()
+    serializer_class = GroupRecordSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
+    
+
+@extend_schema_view(
+    get=extend_schema(
+        summary="Get a list of all coments",
+    ),
+    post=extend_schema(
+        summary="Create a new comment",
+    ),
+)
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(owner=user, created_date=datetime.datetime.now().isoformat())
+    
+
+@extend_schema_view(
+    get=extend_schema(
+        summary="Get comment by id",
+    ),
+    put=extend_schema(
+        summary="Update comment by id",
+    ),
+    patch=extend_schema(
+        summary="Partialy update comment by id",
+    ),
+    delete=extend_schema(
+        summary="Delete comment by id",
+    ),
+)
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentNoParentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
